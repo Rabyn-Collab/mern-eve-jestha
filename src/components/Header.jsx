fa6f5505e4761bcc2f8142a370c1ffd9c7fc299a
Rabyn-Collab <babynshrestha64@gmail.com>
2025-07-27 14:52:26 +0545

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,7 @@
-import { NavLink } from "react-router";
 
 export default function Header() {
   return (
-    <div className="bg-[#F44336] text-white   flex items-baseline shadow-2xl">
-
-      <h1 className="text-2xl bg-white text-black px-2 py-2">Home</h1>
-
-      <nav className="space-x-9">
-        <NavLink className={'hover:bg-white hover:py-[10px] px-5 hover:text-black'} to={'/about'}>Link1</NavLink>
-        <NavLink to={'/contact'}>Link2</NavLink>
-        <NavLink to={'/contact'}>Link3</NavLink>
-        <NavLink to={'/contact'}>Link4</NavLink>
-      </nav>
+    <div>
 
     </div>
   )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,23 @@
-import Article from "./Article.jsx";
-import DisplaySec from "./DisplaySec";
+import { faker } from "@faker-js/faker"
+import { Button } from "@material-tailwind/react";
 
 export default function Home() {
+
+  const handleData = () => {
+    const newObject = {
+      userId: faker.string.uuid(),
+      username: faker.internet.username(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+    }
+    console.log(newObject);
+  }
+
+
   return (
-    <div>
+    <div className="p-5">
+      <Button onClick={handleData}>Click Here</Button>
 
-
-
-
-      <DisplaySec />
-      <Article isSec={false} />
-      <Article isSec={true} />
 
 
     </div>

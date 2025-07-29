78e6ae1c86aa7a712e9aa69a0505b8effc2a6dfb
Rabyn-Collab <babynshrestha64@gmail.com>
2025-07-29 14:56:56 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,22 +1,53 @@
-import { faker } from "@faker-js/faker"
-import { Button } from "@material-tailwind/react";
+import { faker } from "@faker-js/faker";
+import { Button, IconButton } from "@material-tailwind/react";
+import { useState } from "react"
+
 
 export default function Home() {
 
+  const [data, setData] = useState([]);
+
+
   const handleData = () => {
-    const newObject = {
-      userId: faker.string.uuid(),
-      username: faker.internet.username(),
-      email: faker.internet.email(),
-      avatar: faker.image.avatar(),
-    }
-    console.log(newObject);
+    const newData = {
+      id: faker.string.uuid(),
+      name: faker.internet.displayName(),
+      image: faker.image.personPortrait()
+    };
+    setData((prev) => [...prev, newData]);
   }
+
+  const removeData = (id) => {
+
+  }
+
+  console.log(data);
 
 
   return (
     <div className="p-5">
-      <Button onClick={handleData}>Click Here</Button>
+      <Button onClick={handleData}>Click</Button>
+      <div className="grid grid-cols-4 gap-5 mt-5">
+
+
+        {data.map((user, index) => {
+          return <div key={user.id} className="space-y-2">
+            <h1>{user.name}</h1>
+            <img className="h-[250px] w-full object-cover" src={user.image} alt="" />
+            <div className="flex justify-end">
+              <IconButton color="pink" size="sm">
+                <i className="fas fa-trash" />
+              </IconButton>
+            </div>
+
+          </div>
+
+        })}
+
+      </div>
+
+
+
 
 
 

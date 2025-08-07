07b6223ebca03784e8b38d83036a0c2f98553082
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-07 14:25:06 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { createBrowserRouter } from "react-router"
 import { RouterProvider } from "react-router-dom";
 import RootLayOut from "./components/RootLayOut";
-import CategoryItems from "./pages/category-items/CategoryItems.jsx";
-import Meal from "./pages/meal/Meal.jsx";
 import Home from "./pages/home/Home.jsx";
-import SearchPage from "./pages/search-meal/SearchPage.jsx";
 
 export default function App() {
 
@@ -18,18 +15,6 @@ export default function App() {
           index: true,
           element: <Home />
         },
-        {
-          path: 'category-items',
-          element: <CategoryItems />
-        },
-        {
-          path: 'meal/:id',
-          element: <Meal />
-        },
-        {
-          path: 'search-meal',
-          element: <SearchPage />
-        }
 
       ]
     },
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,10 @@
-import MealCategory from "../meal-category/MealCategory.jsx";
-import SearchInput from "../search-meal/SearchInput.jsx";
+
 
 export default function Home() {
   return (
     <div>
 
-      <SearchInput />
 
-      <MealCategory />
 
 
     </div>

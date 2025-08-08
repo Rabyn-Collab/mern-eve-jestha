901307bf44f5e4d0f4e34960e2b6182730d1c619
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-08 14:52:10 +0545

    changes

--- src/pages/users/UserAddForm.jsx
+++ src/pages/users/UserAddForm.jsx
@@ -1,16 +1,20 @@
-import { Button, Input, Typography } from "@material-tailwind/react";
+import { Button, Checkbox, Input, Option, Radio, Select, Textarea, Typography } from "@material-tailwind/react";
 import { Formik } from "formik";
+import * as Yup from 'yup';
 
 
-
+const valSchema = Yup.object({
+  username: Yup.string().min(5).max(50).required(),
+  email: Yup.string().matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'please provide valid email').required(),
+  gender: Yup.string().required(),
+  habits: Yup.array().min(1).required(),
+  country: Yup.string().required(),
+  bio: Yup.string().required(),
+  // image: ''
+})
 
 export default function UserAddForm() {
 
-  // const person = {
-  //   name: 'ram',
-  //   age: 90
-  // };
-  // const { name, age } = person;
 
   return (
     <div className="p-5">
@@ -19,15 +23,21 @@ export default function UserAddForm() {
       <Formik
         initialValues={{
           username: '',
-          email: ''
+          email: '',
+          gender: '',
+          habits: [],
+          country: '',
+          bio: '',
+          image: ''
         }}
         onSubmit={(val, { resetForm }) => {
           console.log(val);
           // resetForm();
         }}
+        validationSchema={valSchema}
 
       >
-        {({ handleChange, handleSubmit, values }) => {
+        {({ handleChange, handleSubmit, values, setFieldValue, errors, touched }) => {
 
           return <form
             onSubmit={handleSubmit}
@@ -37,6 +47,7 @@ export default function UserAddForm() {
                 value={values.username}
                 onChange={handleChange}
                 label="Username" name="username" />
+              {errors.username && touched.username && <h1 className="text-pink-700">{errors.username}</h1>}
 
             </div>
 
@@ -45,7 +56,60 @@ export default function UserAddForm() {
                 value={values.email}
                 onChange={handleChange}
                 label="Email" type="email" name="email" />
+              {errors.email && touched.email && <h1 className="text-pink-700">{errors.email}</h1>}
             </div>
+
+            <div>
+              <Typography>Select your Gender</Typography>
+              <Radio
+                color="indigo"
+                onChange={handleChange}
+                label='Male' value={'male'} name="gender" />
+              <Radio
+                color="purple"
+                onChange={handleChange}
+                label='Female' value={'female'} name="gender" />
+              {errors.gender && touched.gender && <h1 className="text-pink-700">{errors.gender}</h1>}
+            </div>
+
+            <div>
+              <Typography>Select your Habits</Typography>
+              <Checkbox
+                onChange={handleChange}
+                label='Dance' value={'dance'} name="habits" />
+              <Checkbox
+                onChange={handleChange}
+                label='Sing' value={'sing'} name="habits" />
+              {errors.habits && touched.habits && <h1 className="text-pink-700">{errors.habits}</h1>}
+
+            </div>
+
+            <div>
+              <Select
+                onChange={(e) => setFieldValue('country', e)}
+                name="country"
+                label="Select Your Country">
+                <Option value="Nepal">Nepal</Option>
+                <Option value="India">India</Option>
+                <Option value="China">China</Option>
+              </Select>
+              {errors.country && touched.country && <h1 className="text-pink-700">{errors.country}</h1>}
+            </div>
+
+            <div>
+              <Textarea name="bio" onChange={handleChange} label="Enter your Bio"></Textarea>
+              {errors.bio && touched.bio && <h1 className="text-pink-700">{errors.bio}</h1>}
+            </div>
+
+            {/* <Input
+              onChange={(e) => {
+                const file = e.target.files[0];
+                setFieldValue('image', URL.createObjectURL(file));
+              }}
+              label="Select An Image" type="file" name="image" /> */}
+            {/* <h1>{values.image}</h1> */}
+
+            {/* {values.image && <img src={values.image} alt="" />} */}
 
             <Button type="submit">Submit</Button>
 

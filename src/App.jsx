ceffcba963b7b2056389261d552217369015ddcf
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-14 14:19:24 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,10 @@ import { createBrowserRouter } from "react-router"
 import { RouterProvider } from "react-router-dom";
 import RootLayOut from "./components/RootLayOut";
 import Home from "./pages/home/Home.jsx";
-import UserAddForm from "./pages/users/UserAddForm.jsx";
-import UserUpdateForm from "./pages/users/UserUpdateForm.jsx";
 
 export default function App() {
 
-  // const persons = [
-  //   { id: 1, name: 'ram' },
-  //   { id: 2, name: 'shyam' },
-  //   { id: 3, name: 'hari' },
-  // ];
 
-  // const per = persons.map((person) => {
-  //   return person.id === 1 ? { id: 1, name: 'rita' } : person
-  // });
-
-  // console.log(per);
 
   const router = createBrowserRouter([
 
@@ -29,14 +17,7 @@ export default function App() {
           index: true,
           element: <Home />
         },
-        {
-          path: 'add-user',
-          element: <UserAddForm />
-        },
-        {
-          path: 'update-user/:id',
-          element: <UserUpdateForm />
-        }
+
 
       ]
     },
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,60 +1,7 @@
-import { IconButton, Typography } from "@material-tailwind/react";
-import { useDispatch, useSelector } from "react-redux"
-import { removeUser } from "../users/userSlice.js";
-import { useNavigate } from "react-router";
-
 
 export default function Home() {
-
-  const { users } = useSelector((state) => state.userSlice);
-  const dispatch = useDispatch();
-  const nav = useNavigate();
-
-  // console.log(users);
-
-  // const person = {
-  //   name: 'ram',
-  //   age: 90
-  // };
-
-  // console.log({ ...person, id: 1 });
-
   return (
-    <div className="p-5 grid grid-cols-4 gap-5">
-
-      {users.map((user, index) => {
-        return <div key={user.id}>
-          <h1 className="font-bold">{user.username}</h1>
-          <p className="italic">{user.email}</p>
-          <p>Gender: {user.gender}</p>
-          <p>Country: {user.country}</p>
-          <Typography variant="lead">Habits</Typography>
-          <div className="flex gap-4">
-            {user.habits.map((hab, i) => {
-              return <Typography color="brown" key={i}>{hab}</Typography>
-            })}
-          </div>
-          <Typography variant="paragraph">Bio</Typography>
-          <p className="text-gray-700 text-sm">{user.bio}</p>
-
-          <div className="flex mt-2 justify-end gap-5">
-            <IconButton
-              onClick={() => nav(`/update-user/${user.id}`)}
-              size="sm" color="green">
-              <i className="fas fa-edit" />
-            </IconButton>
-            <IconButton
-              onClick={() => dispatch(removeUser(index))}
-              size="sm" color="pink">
-              <i className="fas fa-trash" />
-            </IconButton>
-          </div>
-
-
-        </div>
-
-      })}
-
+    <div>
 
     </div>
   )

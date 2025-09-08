5f1ffe29c4cd779f900a2789a778e94d54b9b4ed
Rabyn-Collab <babynshrestha64@gmail.com>
2025-09-08 14:31:04 +0545

    changes

---
+++ middleware/authCheck.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+
+
+export const checkUser = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  const decode = jwt.decode(token, 'secret');
+  if (!decode) return res.status(401).json({ message: 'you are not authorized ' });
+  req.userId = decode.id;
+  req.role = decode.role;
+  next();
+}
+
+
+export const checkAdmin = (req, res, next) => {
+  if (req.role === 'Admin') return next();
+  return res.status(401).json({ message: 'you are not authorized ' });
+}

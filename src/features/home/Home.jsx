9a7c6ca96047a7ab2c171b96d67c9e551daaaf14
Rabyn-Collab <babynshrestha64@gmail.com>
2025-09-28 14:30:19 +0545

    changes

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,7 +1,10 @@
+
 import ProductList from "../products/ProductList";
 import SearchInput from "../search/SearchInput";
 
 export default function Home() {
+
+
   return (
     <div className="p-5">
 
--- src/features/products/ProductList.jsx
+++ src/features/products/ProductList.jsx
@@ -1,41 +1,62 @@
-import { Card, CardBody, CardFooter, Image } from "@heroui/react";
+import { Button, Card, CardBody, CardFooter, Image } from "@heroui/react";
 import { useGetProductsQuery } from "./productApi";
 import { base } from "../../app/mainApi";
-import { useNavigate } from "react-router";
+import { useNavigate, useSearchParams } from "react-router";
+import { useEffect } from "react";
 
 export default function ProductList() {
-  const { isLoading, error, data } = useGetProductsQuery();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { isLoading, error, data } = useGetProductsQuery({
+    page: searchParams.get('page') ?? 1
+  });
   const nav = useNavigate();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [searchParams]);
+
 
 
   if (isLoading) return <h1>Loading....</h1>
   if (error) return <h1 className="text-red-500">{error.message}</h1>
 
+
   return (
-    <div className="gap-5 grid grid-cols-2 sm:grid-cols-4">
+    <div>
+      <div className="gap-5 grid grid-cols-2 sm:grid-cols-4">
 
-      {data && data?.products.map((item, index) => (
+        {data && data?.products.map((item, index) => (
 
-        <Card
+          <Card
 
-          key={index} isPressable shadow="sm" onPress={() => nav(`/product/${item._id}`)}>
-          <CardBody className="overflow-visible p-0">
-            <Image
-              alt={item.title}
-              className="w-full object-cover h-[140px]"
-              radius="lg"
-              shadow="sm"
-              src={`${base}/${item.image}`}
-              width="100%"
-            />
-          </CardBody>
-          <CardFooter className="text-small justify-between">
-            <b>{item.title}</b>
-            <p className="text-default-500">Rs. {item.price}</p>
-          </CardFooter>
-        </Card>
-      ))}
+            key={index} isPressable shadow="sm" onPress={() => nav(`/product/${item._id}`)}>
+            <CardBody className="overflow-visible p-0">
+              <Image
+                alt={item.title}
+                className="w-full object-cover h-[140px]"
+                radius="lg"
+                shadow="sm"
+                src={`${base}/${item.image}`}
+                width="100%"
+              />
+            </CardBody>
+            <CardFooter className="text-small justify-between">
+              <b>{item.title}</b>
+              <p className="text-default-500">Rs. {item.price}</p>
+            </CardFooter>
+          </Card>
+        ))}
 
+      </div>
+      <div className="flex justify-center items-center gap-5 my-3">
+        <Button
+          onPress={() => setSearchParams({ page: Number(data.page) - 1 })}
+          disabled={Number(data.page) === 1}>Prev</Button>
+        <h1>{data.page}</h1>
+        <Button
+          onPress={() => setSearchParams({ page: Number(data.page) + 1 })}
+          disabled={Number(data.page) === data.totalPages}
+        >Next</Button>
+      </div>
     </div>
   );
 }

086019ead139d31c20716a96481eb521444324bb
Rabyn-Collab <babynshrestha64@gmail.com>
2025-10-06 15:00:46 +0545

    changes

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -1,4 +1,8 @@
 
 
-export const base = 'http://192.168.1.116:5000';
-export const baseUrl = 'http://192.168.1.116:5000/api';
+// export const base = 'http://192.168.1.116:5000';
+// export const baseUrl = 'http://192.168.1.116:5000/api';
+
+export const base = 'https://mern-eve-jestha.onrender.com';
+export const baseUrl = 'https://mern-eve-jestha.onrender.com/api';
+

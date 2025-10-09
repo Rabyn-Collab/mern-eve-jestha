6e0acbc23c7bda8cba8a9f09534aa3a5f7bbccbd
Rabyn-Collab <babynshrestha64@gmail.com>
2025-10-09 15:02:43 +0545

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import { Button } from "../components/ui/button";
 
 export default function Page() {
   return (
-    <div>
+    <div className="p-5">
+      <Button className={'bg-red-500 px-10'}>Click Me</Button>
       <h1>Hello jee</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, unde iste cum excepturi animi nemo et. Nobis provident labore in voluptatem quasi, tenetur nesciunt vel. Tempora aperiam necessitatibus nemo ab.</p>
 

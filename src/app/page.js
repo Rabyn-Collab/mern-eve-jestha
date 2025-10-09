fa3bade1219eb6231d93f8b5f5ea5cd8b62a3e1e
Rabyn-Collab <babynshrestha64@gmail.com>
2025-10-09 14:53:14 +0545

    changes

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello jee</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, unde iste cum excepturi animi nemo et. Nobis provident labore in voluptatem quasi, tenetur nesciunt vel. Tempora aperiam necessitatibus nemo ab.</p>
+
+    </div>
+  )
+}
